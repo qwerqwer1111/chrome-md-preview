67c6112481bd494a67a2a10ffd9fd49acf03b8e7
qwerqwer1111 <qwerqwer1111+github@gmail.com>
2018-09-23 18:01:20 +0900

    Update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,30 @@
 const webpack = require('webpack');
-const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/main.js'),
+  entry: './src/main.js',
+
   output: {
-    path: path.resolve(__dirname, './build/js'),
+    path: `${__dirname}/build/js`,
     filename: 'bundle.js'
   },
+
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
-        test: /\.svg$/,
-        loader: 'url-loader?mimetype=image/svg+xml'
-      },
-      {
-        test: /\.woff$/,
-        loader: 'url-loader?mimetype=application/font-woff'
-      },
-      {
-        test: /\.woff2$/,
-        loader: 'url-loader?mimetype=application/font-woff'
-      },
-      {
-        test: /\.eot$/,
-        loader: 'url-loader?mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf$/,
-        loader: 'url-loader?mimetype=application/font-woff'
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
       }
     ]
   },
+
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
