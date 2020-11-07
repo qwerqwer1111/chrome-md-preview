28f2c3d52a96c38364e1ea84778127ce45f1a014
qwerqwer1111 <qwerqwer1111+github@gmail.com>
2020-11-07 16:25:23 +0900

    Refactor

--- src/main.js
+++ src/main.js
@@ -2,42 +2,40 @@ import * as hljs from 'highlight.js';
 import * as MarkdownIt from 'markdown-it';
 import './main.css';
 
-function render(text) {
-  const option = {
-    html: true,
-    linkify: true,
-    highlight: (str, lang) => {
-      if (lang && hljs.getLanguage(lang)) {
-        try {
-          return hljs.highlight(lang, str).value;
-        } catch (__) {}
-      }
-      return '';
-    },
-  };
-  document.body.innerHTML = new MarkdownIt(option)
-    .use(require('markdown-it-task-lists'))
-    .use(require('markdown-it-texmath'), {
-      engine: require('katex'),
-      delimiters: 'dollars',
-    })
-    .render(text);
-}
+const option = {
+  html: true,
+  linkify: true,
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+    return '';
+  },
+};
+
+const md = new MarkdownIt(option)
+  .use(require('markdown-it-task-lists'))
+  .use(require('markdown-it-texmath'), {
+    engine: require('katex'),
+    delimiters: 'dollars',
+  });
 
 (() => {
   document.body.classList.add('markdown-body');
   let prev = document.body.innerText;
-  render(prev);
+  document.body.innerHTML = md.render(prev);
   setInterval(() => {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', location.href, true);
     xhr.onload = () => {
-      const data = xhr.responseText;
-      if (prev === data) {
+      const text = xhr.responseText;
+      if (prev === text) {
         return;
       }
-      prev = data;
-      render(data);
+      prev = text;
+      document.body.innerHTML = md.render(text);
     };
     xhr.send();
   }, 1000);
